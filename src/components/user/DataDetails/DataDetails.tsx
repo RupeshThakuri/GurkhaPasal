e34fea37f50f45f835cd529947319b514991f8ac
RupeshThakuri <rupeshthakuri16@gmail.com>
2025-03-28 14:54:00 +0545

    update 2

---
+++ src/components/user/DataDetails/DataDetails.tsx
@@ -0,0 +1,26 @@
+type Product = {
+    id: number;
+    name: string;
+    price: number;
+    image: string;
+    description: string;
+    discount: string;
+};
+
+const productData: Product[] = [
+ 
+ 
+ 
+
+    { id: 1, name: "Traditional Dhaka Topi", price: 1200, image: '/Image/User/cod.png', description: "Authentic Nepali Dhka Topi, handcrafted with traditional patterns.", discount: "20% Off" },
+    { id: 2, name: "Handmade Pashmina Shawl", price: 3500, image: '/Image/User/b2.jpeg', description: "Premium quality pashmina shawl made from 100% cashmere wool.", discount: "20% Off" },
+    { id: 3, name: "Himalayan Rock Salt", price: 450, image: '/Image/User/b3.jpeg', description: "Pure Himalayan pink salt, rich in minerals and natural flavor.", discount: "20% Off" },
+    { id: 4, name: "Organic Nepal Tea 1", price: 850, image: '/Image/User/b4.jpeg', description: "Premium organic black tea from the hills of Nepal.", discount: "20% Off" },
+    { id: 5, name: "Organic Nepal Tea 2", price: 850, image: '/Image/User/b4.jpeg', description: "Premium organic black tea from the hills of Nepal.", discount: "20% Off" },
+    { id: 6, name: "Organic Nepal Tea 3", price: 850, image: '/Image/User/b4.jpeg', description: "Premium organic black tea from the hills of Nepal.", discount: "20% Off" },
+    { id: 7, name: "Organic Nepal Tea 4", price: 850, image: '/Image/User/b4.jpeg', description: "Premium organic black tea from the hills of Nepal.", discount: "20% Off" },
+    { id: 8, name: "Organic Nepal Tea 5", price: 850, image: '/Image/User/b4.jpeg', description: "Premium organic black tea from the hills of Nepal.", discount: "20% Off" },
+    { id: 9, name: "Organic Nepal Tea 6", price: 850, image: '/Image/User/b4.jpeg', description: "Premium organic black tea from the hills of Nepal.", discount: "20% Off" },
+    { id: 10, name: "Organic Nepal Tea 7", price: 850, image: '/Image/User/b4.jpeg', description: "Premium organic black tea from the hills of Nepal.", discount: "20% Off" },
+];
+export default productData;
